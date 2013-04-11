d699f4e5795a19e9ac90ade4214ac81dbd95bf88
jonny <lutz.kwauka@payango.com>
2013-04-11 12:22:23 +0200

    add locale scope to localeapp_i18n_js routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope :module => LocaleappI18nJs do
-    post "localeapp_i18n_js", :to => "LocaleappI18nJs#update"
-    get "localeapp_i18n_js", :to => "LocaleappI18nJs#show"
+    scope "(:locale)", :locale => /de|en/ do
+      post "localeapp_i18n_js", :to => "LocaleappI18nJs#update"
+      get "localeapp_i18n_js", :to => "LocaleappI18nJs#show"
+    end
   end
 end
 
